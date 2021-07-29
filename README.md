2c332708581de0cd974b4c502df3b5bc31674cd9
Micky <ad312@pm.me>
2021-07-29 18:11:07 +1000

    working model

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # csgo-afk-controller
 A script to handle auto gun drops amongst other things
+
+## Usage
+1. Enter this command `con_logfile "console.log"` into either your console each time or into autoexec. 
+2. Edit the logLocation variable to match your current csgo installation directory.
+Example = `D:\Games\Steam\steamapps\common\Counter-Strike Global Offensive\csgo\console.log"`
+3. When you need to be afk, run the script and open the console in game. (Must be tabbed into the game)
--- main.py
+++ main.py
@@ -1,22 +1,57 @@
-import shutil
-import os
+import pyautogui
 
-#Config
-#con_logfile "consolelog.log"
+logLocation = "D:\Games\Steam\steamapps\common\Counter-Strike Global Offensive\csgo\console.log"
 
-#pyautogui.write('Hello There')
-logLocation = "D:\Games\Steam\steamapps\common\Counter-Strike Global Offensive\csgo\consolelog.log"
+gunList = {
+    "ak": "ak47",
+    "m4": "m4a4",
+    "m4-s": "m4a1_silencer",
+    "deagle": "deagle",
+    "five7": "fiveseven",
+    "mp9": "mp9",
+    "p90": "p90",
+    "ssg": "ssg08",
+    "xm": "xm1014",
+    "negev": "negev"
+}
 
 def getDropLine():
-        datafile = open(logLocation, encoding='utf-8')
-        for line in datafile:
-            if "!drop" in line:
-                checkGun(line)
+        lines = list(open(logLocation, "r", encoding='utf-8'))
+        lastLines = lines[-20:]
+        for line in lastLines:
+            if "!drop" in line: 
+                checkGun(line.partition("!drop")[2].strip(), line.partition("!drop")[0][:-2])
                 break
+            elif "!help" in line:
+                getHelp()
 
-def checkGun(line):
-    print(line)
+
+def checkGun(gun, requester):   
+    dropGun = None
+    if gun in gunList:
+        dropGun = gunList[gun]
+    
+    if dropGun:
+        print(f"{gun} was requested by {requester}")
+        pyautogui.write(f"buy {dropGun}")
+        pyautogui.press('enter')
+    
+    else:
+        print("Invalid gun selection")
+        pyautogui.write(f"say Invalid gun selection. Try !help")
+        pyautogui.press('enter')
+
+
+def getHelp():
+    pyautogui.write("say Usage: !drop gun")
+    pyautogui.press('enter')
+    pyautogui.write("say Available guns to drop are:")
+    pyautogui.press('enter')
+    gunString = ""
+    for key in gunList:
+        gunString += ", " + key
+        
+    pyautogui.write(f"say {gunString}")
+    pyautogui.press('enter')
 
 getDropLine()
-
-    

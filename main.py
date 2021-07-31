75669589b05260fc078c097be44baac6bdcd25f5
Micky <ad312@pm.me>
2021-07-31 21:00:13 +1000

    fixes

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ logLocation = "D:\Games\Steam\steamapps\common\Counter-Strike Global Offensive\c
 scheduler = sched.scheduler(time.time, time.sleep)
 requestedDrops = []
 requestedHelp = []
+bombDropped = False
+currentSeconds = 0
 gunList = {
     "ak": "ak47",
     "m4": "m4a1",
     "m4-s": "m4a1_silencer",
     "deagle": "deagle",
+    "p250": "p250",
     "five7": "fiveseven",
     "mp9": "mp9",
     "p90": "p90",
@@ -20,35 +23,43 @@ gunList = {
 
 
 def getDropLine():
-        lines = list(open(logLocation, "r", encoding='utf-8'))
-        lastLines = lines[-20:]
-        for line in lastLines:
-            gun = line.partition("!drop")[2].strip()
-            player = line.partition("!drop")[0][:-2]
-            if "!drop" in line and player not in requestedDrops: 
-                checkGun(gun, player)
-                break
-            elif "!help" in line and player not in requestedHelp:
-                getHelp()
-                requestedHelp.append(player)
-        print("Checked for requests")
+    global currentSeconds
+    if currentSeconds > 30:
+        currentSeconds = 0
+        resetRoundChecks()
+
+    lines = list(open(logLocation, "r", encoding='utf-8'))
+    lastLines = lines[-20:]
+    for line in lastLines:
+        gun = line.partition("!drop")[2].strip()
+        player = line.partition("!drop")[0][:-2]
+        if "!drop" in line and player not in requestedDrops: 
+            checkGun(gun, player)
+            break
+        elif "!help" in line and player not in requestedHelp:
+            getHelp()
+            requestedHelp.append(player)
+    currentSeconds += 1
+    print("Checked for requests")
 
 
 def checkGun(gun, player):   
+    global bombDropped
     dropGun = None
     if gun in gunList:
         dropGun = gunList[gun]
     
+    elif gun == "bomb" or "c4" and bombDropped == False:
+        print(f"The bomb was dropped by {player}")
+        bombDropped = True
+        keyboard.write(f"use weapon_c4;drop")
+        keyboard.press_and_release('enter')
+    
     if dropGun:
         print(f"{gun} was requested by {player}")
-        keyboard.write(f"buy {dropGun}; drop")
-        keyboard.press_and_release('enter')
         requestedDrops.append(player)
-    
-    else:
-        print("Invalid gun selection")
-        keyboard.write(f"say_team Invalid gun selection. Try !help")
-        keyboard.press('enter')
+        keyboard.write(f"buy {dropGun};drop")
+        keyboard.press_and_release('enter')
 
 
 def getHelp():
@@ -61,14 +72,18 @@ def getHelp():
     keyboard.press('enter')
 
 
-def resetPlayerArrays():
+def resetRoundChecks():
+    global bombDropped
+    print("Round checks cleared")
     requestedDrops.clear()
     requestedHelp.clear()
+    bombDropped = False
+    
 
-
-print("AFK Controller Initialised")
-keyboard.write(f"say_team AFK Controller Initialised")
+print("AFK Controller Initialised. Make sure to tab into the game and open the console.")
+time.sleep(5)
+keyboard.write(f"say_team AFK Controller Initialised. Try !help for additional information;+right")
+keyboard.press('enter')
 while True:
     e1 = scheduler.enter(1, 1, getDropLine)
-    e2 = scheduler.enter(20, 1, resetPlayerArrays)
     scheduler.run()
